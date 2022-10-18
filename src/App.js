15aa690f6c4d6214870c7bcf71a237058774d143
Woncheol Kim <icijiy@gmail.com>
2022-10-18 15:40:58 -0700

    Add componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count : 0
+    isLoading: true
   };
-  add = () => {};
-  minus = () => {};
+  conponentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
   render() {
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    )
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
