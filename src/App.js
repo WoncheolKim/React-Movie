b42e9c3155492dd717f982efd4c3e1b939b56386
Woncheol Kim <icijiy@gmail.com>
2022-10-18 17:57:00 -0700

    Styling the Movies

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movies: [],
+    movies: []
   };
   getMovies = async () => {
     const {
       data: {
-        data: { movies },
-      },
+        data: { movies }
+      }
     } = await axios.get(
       "https://yts-proxy.nomadcoders1.now.sh/list_movies.json"
     );
@@ -23,20 +24,27 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map((movie) => (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : ( 
+          <div class="movies">
+            {movies.map(movie => (
               <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
               />
             ))}
-      </div>
+
+          </div>
+        )}
+      </section>
     );
   }
 }
