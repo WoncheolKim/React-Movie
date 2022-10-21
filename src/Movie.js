04f0f1175f070d640c3d1457e05f1475e5be1731
Woncheol Kim <icijiy@gmail.com>
2022-10-20 20:55:38 -0700

    Add props generes

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
     <div class="movie">
       <img src={poster} alt={title} title={title} />
@@ -20,7 +20,8 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
